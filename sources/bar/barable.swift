f23f179a1e1cd3130255a95052dadd362347dae9
kelvin13 <kelvin13ma@gmail.com>
2022-06-29 12:31:04 -0500

    second commit

--- sources/bar/barable.swift
+++ sources/bar/barable.swift
@@ -1,5 +1,16 @@
 /// A passive test protocol.
+/// 
+/// In version `0.2.0` of this package, ``Barable`` gained a method called 
+/// ``bar(_:)``.
 public 
 protocol Barable
 {
+}
+extension Barable 
+{
+    /// This method was introduced in version `0.2.0` of ``/swift-bar``.
+    public 
+    func bar() 
+    {
+    }
 }
