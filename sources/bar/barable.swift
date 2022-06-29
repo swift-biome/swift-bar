9e52921e9084c029e5334c54b94fa17213f6520b
kelvin13 <kelvin13ma@gmail.com>
2022-06-29 12:34:47 -0500

    fix broken symbollink

--- sources/bar/barable.swift
+++ sources/bar/barable.swift
@@ -1,7 +1,7 @@
 /// A passive test protocol.
 /// 
 /// In version `0.2.0` of this package, ``Barable`` gained a method called 
-/// ``bar(_:)``.
+/// ``Barable/.bar(_:)``.
 public 
 protocol Barable
 {
